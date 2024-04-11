5931f5f199f0c6674e0eb9f38c5af12dba77d044
Judson <Braden.Judson@dfo-mpo.gc.ca>
2024-04-11 09:15:29 -0700

    test commit

--- genotypes/n_reads/n_reads.R
+++ genotypes/n_reads/n_reads.R
@@ -2,5 +2,4 @@ setwd("~/sockeye_lcwgs/genotypes/n_reads")
 
 library(tidyverse)
 
-reads <-  read.table("all_flagstat.txt",
-                     sep = "\t")
+reads <-  read.table("all_flagstat.txt", sep = "\t")
